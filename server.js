e1d57e94544e86691d720efe544d6c4a004ea1ba
jasonkunda <jasonkunda00@gmail.com>
2022-09-01 23:26:57 -0700

    core features added

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express();
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 
-
 app.use(express.static(__dirname + '/views'));
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
@@ -18,14 +17,47 @@ app.use(express.urlencoded({extended: false})); //Parse URL-encoded bodies
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+
+
+function get_title(url){
+    ytdl.getInfo(url)
+.then(info => {
+    return info.videoDetails.title;});
+};
+
 app.get('/', (req, res) => {
     res.render("index");
-
 });
 
-app.post('/', function (req, res) {
-    res.render('index', { name: req.body.name });
+app.get('/download', (req, res) => { 
+    res.render("download");
+});
+
+
+
+app.post('/', (req, res) => {
+    
     console.log(req.body.url);
+        console.log('valid url')
+    let v_id = req.body.url.split('/')[3];
+
+    
+    // console.log(get_title());
+    if (ytdl.validateURL(req.body.url))
+    {
+        ytdl(req.body.url, {quality:"highest"})
+        .pipe(fs.createWriteStream(__dirname + `/views/videos/video.mp4`), function(){
+            res.render("loader")
+        })
+        .on("close", function(){
+            console.log("done downlaoding");
+            res.render('download', { url: req.body.url });
+        })
+    }
+    else{
+        res.render('index')
+    }
+
 
 });
 
--- server/videos/download.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def downlaod_video(url):
-    os.system(f"youtube-dl --output my_webapp/videos/%(title)s.%(ext)s -f mp4 {url}")
-downlaod_video("https://youtu.be/x7X9w_GIm1s")
